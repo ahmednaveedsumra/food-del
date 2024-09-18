2068ac3047f61010cb5b8ad88e930a79e02c5380
Ahmad Naveed <143949511+ahmednaveedsumra@users.noreply.github.com>
2024-09-18 22:53:47 +0500

    Initial commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.</p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+
+                </div>
+                <div className="footer-content-center">
+                    <h2>Company</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+
+                </div>
+                <div className="footer-content-right">
+                    <h2>Get in touch</h2>
+                    <ul>
+                        <li>+1-23456-789</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+
+
+
+                </div>
+
+
+
+
+
+
+
+            </div>
+
+            <hr />
+            <p className="footer-copyright">Copyright 2024 @ Tomato.com - All Right Reserved.</p>
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Our diverse menu offers a delicious range of options, perfect for every taste. Begin with a fresh and vibrant Salad or savor the crisp flavors of our signature Rolls. For something sweet, indulge in our delightful Deserts or a slice of rich, decadent Cake. If you’re in the mood for a light bite, try our mouthwatering Sandwich, or opt for a hearty dish like our creamy Pasta or flavorful Noodles. We also offer a satisfying Pure Veg option, ensuring there’s something for everyone to enjoy.</p>
+            <button>View Menu</button>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Header
