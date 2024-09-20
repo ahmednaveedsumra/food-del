0ac5945b09d62e0a9627e8e3c660a21629b7afb2
Ahmad Naveed <143949511+ahmednaveedsumra@users.noreply.github.com>
2024-09-20 20:31:28 +0500

    intial commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import SideBar from './components/SideBar/SideBar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+
+const App = () => {
+  return (
+    <div>
+
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <SideBar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+
+        </Routes>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
